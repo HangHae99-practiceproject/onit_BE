8cbb07e006ba7ac64a6e3da353ef8dcb1dfc7d70
bigtyno931128 <bigtyno4@gmail.com>
2022-04-25 21:59:00 +0900

    ..

---
+++ src/main/java/com/onit_be/onit_be/OnitBeApplication.java
@@ -0,0 +1,13 @@
+package com.onit_be.onit_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnitBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onit_be/onit_be/OnitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onit_be.onit_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnitBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
